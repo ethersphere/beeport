de959f50d15b81f8550097439bae2ea0e0ef4ece
CardinalError <adriadrop@gmail.com>
2025-03-13 22:04:42 +0100

    remove locals from cors

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://swarming.site']
+    origin: ['https://swarming.site']
 }));
 
 const gnosisPublicClient = createPublicClient({
