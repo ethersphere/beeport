f002d72dd98d04bd4463f7cf27cf8555502848cd
CardinalError <adriadrop@gmail.com>
2025-04-04 12:24:22 +0200

    add matomo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/global.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { Providers } from "./providers";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Swarm Storage",
@@ -15,6 +16,23 @@ export const metadata = {
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="matomo-analytics" strategy="afterInteractive">
+          {`
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://mtm.swarm.foundation/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '19']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}
+        </Script>
+      </head>
       <body className="swarm-theme">
         <div className="main-container">
           <video autoPlay muted playsInline className="background-video">
